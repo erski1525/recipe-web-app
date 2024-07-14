ecc4588677127c04a6419fb231e996429e337a6e
Eryk Ciesielski <eryk@192.168.0.106>
2024-07-14 20:28:07 +0200

    add recipes styles

--- src/views/RecipesView.vue
+++ src/views/RecipesView.vue
@@ -1,3 +1,9 @@
+<script setup>
+import ClockIcon from '../components/icons/ClockIcon.vue';
+import CartIcon from '../components/icons/CartIcon.vue';
+import PersonIcon from '../components/icons/PersonIcon.vue';
+import SearchIcon from '../components/icons/SearchIcon.vue';
+</script>
 <template>
     <div class="container">
         <nav class="nav">
@@ -21,38 +27,91 @@
         </ul>
         </nav>
         <div class="container__content">
-            <form class="search">
+            <div>
+                <form class="search">
                 <input class="search__input">
-                <button class="search__submit">search</button>
-            </form>
-            <h3>Recipes (26)</h3>
+                <button class="search__submit"><SearchIcon/></button>
+                </form>
             <ul class="recipes__list">
-                <li class="recipes__item">
-                    Pizza
+                <li>
+                    <div class="recipe">
+                        <div class="recipe__img"></div>
+                        <p class="recipe__title">Pizza</p>
+                        <div class="recipe__description">
+                            <div class="icon__wrapper">
+                                <ClockIcon/>
+                                20min
+                            </div>
+                            <div class="icon__wrapper">
+                                <CartIcon/>
+                                12,99 zł
+                            </div>
+                            <div class="icon__wrapper">
+                                <PersonIcon/>
+                                2 osoby
+                            </div>
+                        </div>
+                    </div>
                 </li>
-                <li class="recipes__item">
-                    Pizza
+                <li>
+                    <div class="recipe">
+                        <div class="recipe__img"></div>
+                        <p class="recipe__title">Pizza</p>
+                        <div class="recipe__description">
+                            <div class="icon__wrapper">
+                                <ClockIcon/>
+                                20min
+                            </div>
+                            <div class="icon__wrapper">
+                                <CartIcon/>
+                                12,99 zł
+                            </div>
+                            <div class="icon__wrapper">
+                                <PersonIcon/>
+                                2 osoby
+                            </div>
+                        </div>
+                    </div>
                 </li>
-                <li class="recipes__item">
-                    Pizza
-                </li><li class="recipes__item">
-                    Pizza
-                </li><li class="recipes__item">
-                    Pizza
-                </li><li class="recipes__item">
-                    Pizza
+                <li>
+                    <div class="recipe">
+                        <div class="recipe__img"></div>
+                        <p class="recipe__title">Pizza</p>
+                        <div class="recipe__description">
+                            <div class="icon__wrapper">
+                                <ClockIcon/>
+                                20min
+                            </div>
+                            <div class="icon__wrapper">
+                                <CartIcon/>
+                                12,99 zł
+                            </div>
+                            <div class="icon__wrapper">
+                                <PersonIcon/>
+                                2 osoby
+                            </div>
+                        </div>
+                    </div>
                 </li>
-                <li class="recipes__item">
-                    Pizza
-                </li>
-                <li class="recipes__item">
-                    Pizza
-                </li>
-                <li class="recipes__item">
-                    Pizza
-                </li>
-                <li class="recipes__item">
-                    Pizza
+                <li>
+                    <div class="recipe">
+                        <div class="recipe__img"></div>
+                        <p class="recipe__title">Pizza</p>
+                        <div class="recipe__description">
+                            <div class="icon__wrapper">
+                                <ClockIcon/>
+                                20min
+                            </div>
+                            <div class="icon__wrapper">
+                                <CartIcon/>
+                                12,99 zł
+                            </div>
+                            <div class="icon__wrapper">
+                                <PersonIcon/>
+                                2 osoby
+                            </div>
+                        </div>
+                    </div>
                 </li>
             </ul>
             <div class="pagination"> 
@@ -62,6 +121,7 @@
                 <div>10</div>
                 <button>Next ></button>
             </div>
+        </div>
         </div>
     </div>
 </template>

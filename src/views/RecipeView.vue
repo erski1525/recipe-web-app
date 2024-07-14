725ae0ca81092c39b263359c065d3774e1445f78
Eryk Ciesielski <eryk@192.168.0.106>
2024-07-14 11:09:16 +0200

    create basic layout and styles

---
+++ src/views/RecipeView.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Pizza</h1>
+    <h3>Ingredients</h3>
+    <ul>
+        <li>cheese</li>
+        <li>ham</li>
+        <li>mushrooms</li>
+        <li>corn</li>
+    </ul>
+    <h3>Instructions</h3>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti natus laboriosam voluptates cum voluptate eaque molestias corporis itaque! Corporis omnis deserunt odio quaerat tempore illum magni dignissimos voluptatum, necessitatibus nemo.
+    Tenetur aliquam vel voluptatibus optio dolorem est numquam! Tempora facilis, dolor optio aliquam repudiandae repellendus numquam corporis eligendi vero a voluptatem? Ut fuga minima molestias iste incidunt, repellat beatae? Dolorum?
+    Cupiditate, delectus enim ex deleniti rerum accusantium molestiae error animi neque perspiciatis optio officiis laborum rem laboriosam, doloribus quisquam libero quidem porro ut maiores. Magnam ducimus vitae deserunt ea corporis.
+    Pariatur, nihil tenetur? Iusto, voluptatum totam? Id laboriosam accusamus iure non asperiores dolores perferendis alias veritatis ducimus, tempora, sed reprehenderit quae vero distinctio aliquam. Alias tempora et vitae sunt? Quidem.
+    Et amet, iusto totam, saepe dicta velit autem molestiae error quasi maxime pariatur cupiditate explicabo corporis sit, ipsam nobis rem incidunt alias ex magni laboriosam dolores odio temporibus. Mollitia, doloribus?
+    Repudiandae aperiam earum distinctio atque eius illum harum perspiciatis dignissimos quos totam! Iste, est! Neque necessitatibus rerum earum nihil porro nulla delectus numquam tempora quis quae. Omnis architecto dolorum quam.
+    Explicabo velit voluptatibus, minima, rerum neque dolores sequi nemo rem libero voluptatem ullam sed quod optio dignissimos accusamus eius ducimus labore iste. Ad perspiciatis deleniti voluptatem beatae, placeat velit. Laudantium.
+    Nostrum sequi vero sunt deleniti sit et, rerum saepe commodi, laudantium distinctio consequuntur assumenda exercitationem odio cumque delectus quam iusto error non? In omnis blanditiis eos necessitatibus non explicabo eaque?
+    Vero soluta vitae laudantium ipsam quis, provident non praesentium obcaecati ipsa aperiam, id fuga nobis inventore maiores molestias suscipit officia sapiente laborum beatae nihil placeat odit est accusantium. Iusto, voluptatum!
+    Placeat quam deleniti est perferendis quaerat non exercitationem fugiat aut odit quia illum porro incidunt, consequuntur architecto inventore hic recusandae enim molestias minima accusantium voluptate. Vel tenetur dolor laboriosam. Illum.</p>
+</template>
---
+++ src/views/RecipesView.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="container">
+        <nav class="nav">
+        <h3 class="logo">Logo</h3>
+        <ul class="nav__list">
+            <li class="link">
+                <RouterLink :to="{name: 'recipes'}" class="link">
+                    Recipes
+                </RouterLink>
+            </li>
+            <li class="link">
+                <RouterLink :to="{name: 'recipes'}" class="link">
+                    Favorite
+                </RouterLink>
+            </li>
+            <li class="link">
+                <RouterLink :to="{name: 'recipes'}" class="link">
+                    Settings
+                </RouterLink>
+            </li>
+        </ul>
+        </nav>
+        <div class="container__content">
+            <form class="search">
+                <input class="search__input">
+                <button class="search__submit">search</button>
+            </form>
+            <h3>Recipes (26)</h3>
+            <ul class="recipes__list">
+                <li class="recipes__item">
+                    Pizza
+                </li>
+                <li class="recipes__item">
+                    Pizza
+                </li>
+                <li class="recipes__item">
+                    Pizza
+                </li><li class="recipes__item">
+                    Pizza
+                </li><li class="recipes__item">
+                    Pizza
+                </li><li class="recipes__item">
+                    Pizza
+                </li>
+                <li class="recipes__item">
+                    Pizza
+                </li>
+                <li class="recipes__item">
+                    Pizza
+                </li>
+                <li class="recipes__item">
+                    Pizza
+                </li>
+                <li class="recipes__item">
+                    Pizza
+                </li>
+            </ul>
+            <div class="pagination"> 
+                <button>< Back</button>
+                <input value="1">
+                <div> / </div>
+                <div>10</div>
+                <button>Next ></button>
+            </div>
+        </div>
+    </div>
+</template>

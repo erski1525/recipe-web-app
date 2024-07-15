ac7f4d8e74800263d50120e0b9b9a82288754167
Eryk Ciesielski <eryk@9903@gmail.com>
2024-07-15 19:35:35 +0200

    style recipes

---
+++ src/components/recipe/RecipeCard.vue
@@ -0,0 +1,71 @@
+<script setup>
+import ClockIcon from '../icons/ClockIcon.vue';
+import CartIcon from '../icons/CartIcon.vue';
+import PersonIcon from '../icons/PersonIcon.vue';
+</script>
+<template>
+    <li class="recipe">
+        <img class="recipe__img" src="/src/assets/default.jpg"/>
+        <p class="recipe__title">Pizza</p>
+        <div class="recipe__description">
+            <div class="icon__wrapper">
+                <ClockIcon/>
+                20min
+            </div>
+            <div class="icon__wrapper">
+                <CartIcon/>
+                12,99 zł
+            </div>
+            <div class="icon__wrapper">
+                <PersonIcon/>
+                2 osoby
+            </div>
+        </div>
+    </li>
+</template>
+
+<style scoped>
+
+.recipe{
+    background-color: #FFF;
+    border-radius: .5rem;
+    overflow: hidden;
+    max-width: 600px;
+}
+
+.recipe:hover{
+    cursor: pointer;
+}
+
+.recipe__img{
+    margin-bottom: 1rem;
+    width: 100%;
+    object-fit: cover;
+}
+
+.recipe__title{
+    margin-top: 0;
+    text-align: center;
+    padding-bottom: 1em;
+    border-bottom: 1px solid var(--color-primary);
+}
+
+.recipe__description{
+    padding: 1em;
+    display: flex;
+    justify-content: space-around;
+}
+
+.icon__wrapper{
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    align-items: center;
+}
+
+@media (min-width: 600px) {
+    .recipe{
+        max-width: 30%;
+    }
+}
+</style>
--- src/views/RecipesView.vue
+++ src/views/RecipesView.vue
@@ -1,47 +1,33 @@
 <script setup>
-import ClockIcon from '../components/icons/ClockIcon.vue';
-import CartIcon from '../components/icons/CartIcon.vue';
-import PersonIcon from '../components/icons/PersonIcon.vue';
-import SearchIcon from '../components/icons/SearchIcon.vue';
+import RecipeCard from '../components/recipe/RecipeCard.vue';
 </script>
 <template>
-    <div class="container">
-        <div class="container__content">
-            <div>
-                <form class="search">
-                <input class="search__input">
-                <button class="search__submit"><SearchIcon/></button>
-                </form>
-            <ul class="recipes__list">
-                <li v-for="i in 20">
-                    <div class="recipe">
-                        <img class="recipe__img" src="../assets/default.jpg"/>
-                        <p class="recipe__title">Pizza</p>
-                        <div class="recipe__description">
-                            <div class="icon__wrapper">
-                                <ClockIcon/>
-                                20min
-                            </div>
-                            <div class="icon__wrapper">
-                                <CartIcon/>
-                                12,99 zł
-                            </div>
-                            <div class="icon__wrapper">
-                                <PersonIcon/>
-                                2 osoby
-                            </div>
-                        </div>
-                    </div>
-                </li>
-            </ul>
-            <div class="pagination"> 
-                <button>< Back</button>
-                <input value="1">
-                <div> / </div>
-                <div>10</div>
-                <button>Next ></button>
-            </div>
-        </div>
-        </div>
-    </div>
+    <ul class="recipes__list">
+       <RecipeCard v-for="index in 20"/>
+     </ul>
 </template>
+
+<style scoped>
+
+.recipes__list{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 1rem;
+    padding: 0 1em;
+}
+
+
+@media (min-width: 600px) {
+    .recipes__list{
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+
+    .recipe{
+        max-width: 30%;
+    }
+}
+</style>
